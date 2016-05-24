63b23ac303280471109f8286cf2383bdf940212c
eabdelmoneim <eiman@Eimans-Mac-mini.local>
2016-05-24 17:03:45 -0500

    created stub for soundcloud login

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
-var oauth = require('oauth2-server');
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Soundcloud Analyzer' });
+});
+
+/* soundcloud login */
+router.post('/login', function(req,res,next){
+	var scLogin = req.body.scLogin;
+	console.log(scLogin);
+	
+	res.status(200).send("entry point into authentication with login: " + scLogin);
 });
 
 module.exports = router;
