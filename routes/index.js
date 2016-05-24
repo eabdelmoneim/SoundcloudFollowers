c835db3811d7b08aea22111f7bc3cab58db24818
eabdelmoneim <eiman@Eimans-Mac-mini.local>
2016-05-24 12:51:06 -0500

    initial check in

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var oauth = require('oauth2-server');
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
